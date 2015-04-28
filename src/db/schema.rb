6e25bfbe1e74c37ed54169f202eebc2eada6f911
Cornelius Toole <cornelius.toole@gmail.com>
2015-04-27 19:47:25 -0500

    added migration for locations table

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427211909) do
+ActiveRecord::Schema.define(version: 20150428002637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "woeid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "twitter_trends", force: :cascade do |t|
     t.string   "name"
